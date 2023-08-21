e59638ec264e63fdb8af9ff500e809de056a8662
michelepiccinno <michele.piccinno@gmail.com>
2023-08-21 17:05:31 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Title from "./components/apptitle.vue"
+export default {
+  components: {
+    Title,
+  }
+}
+</script>
+
+<template>
+  <Title>
+
+  </Title>
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  
+}
+</script>
+<template> 
+<h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+h1 {
+  color: black;
+}
+</style>
